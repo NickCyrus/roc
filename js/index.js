68db1b188d2222ed81594d7d5c83c47cdb652d04
NickCyrus <nicklemusduque@gmail.com>
2017-02-19 11:41:47 -0500

    update

--- js/index.js
+++ js/index.js
@@ -4,26 +4,19 @@
     
     document.addEventListener("deviceready",onDeviceReady,false);
     
-    QRScanner.scan(displayContents);
- 
-    function displayContents(err, text){
-      if(err){
-        // an error occurred, or the scan was canceled (error code `6`) 
-      } else {
-        // The scan completed, display the contents of the QR code: 
-        alert(text);
-      }
-    }
-
+   
     function onDeviceReady() {
         pictureSource   = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
         
-        QRScanner.show();
+       // QRScanner.show();
         
     }
 
     
+     QRScanner.getStatus(function(status){
+        alert(status);
+     });
 
     
  
